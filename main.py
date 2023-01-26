da82246b823bdf98c03e892a8a8a278879a2a273
Danila Antonov <danilidze@ymail.com>
2023-01-27 00:18:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print('hey ya!')
-print('hey ya!')
-print('hey ya!')
+a = 'Python is the best programming language in the world'
+
+b = a[5: -6]
+
+print(b)
+
+a = 'Guido van Rossum is the benevolent dictator for life'
+b = a[::3]
+print(b)
+
+a = 'You have a problem with authority, Mr. Anderson.'
+a_l = list(map(a.count, set(a)))
+b = list(zip(set(a), a_l))
+
+print(b)
